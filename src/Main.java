cc08288ac1b677ae3b04906409c252a093061956
Komputer <Daniel200825@wp.pl>
2019-11-12 06:22:24 +0100

    k

--- src/Main.java
+++ src/Main.java
@@ -6,62 +6,77 @@ public class Main {
     private Float[][] a = new Float[size][size];
     private Float[] b = new Float[size];
     private Float[] x = new Float[size];
-    public void fill(){
+
+    public void fill() {
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                System.out.println("podaj wartosc "+i+" "+j+": ");
+                System.out.println("podaj wartosc " + i + " " + j + ": ");
                 a[i][j] = scanner.nextFloat();
             }
-            System.out.println("podaj wartosc b "+i+": ");
-            b[i]=scanner.nextFloat();
+            System.out.println("podaj wartosc b " + i + ": ");
+            b[i] = scanner.nextFloat();
         }
     }
-    public void fillRandom(){
+
+    public void fillRandom() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                a[i][j] = ThreadLocalRandom.current().nextFloat()*ThreadLocalRandom.current().nextInt(1,20);
+                a[i][j] = ThreadLocalRandom.current().nextFloat() * ThreadLocalRandom.current().nextInt(1, 20);
             }
-            b[i]= ThreadLocalRandom.current().nextFloat()*ThreadLocalRandom.current().nextInt(1,20);
+            b[i] = ThreadLocalRandom.current().nextFloat() * ThreadLocalRandom.current().nextInt(1, 20);
         }
     }
-    public void zeroes(){
+
+    public void zeroes() {
         for (int i = 0; i < size; i++) {
-            for (int j = i+1; j < size; j++) {
-                a[i][j]=0f;
+            for (int j = i + 1; j < size; j++) {
+                a[i][j] = 0f;
             }
         }
     }
-     public void calculate(){
-         for (int i = 0; i < size; i++) {
-             x[i] = b[i]/a[i][i];
-             for (int j = i+1; j < size; j++) {
-                 b[j] = b[j] - a[j][i] * x[i];
-             }
-         }
-     }
-     public void getA(){
-         for (int i = 0; i < size; i++) {
-             for (int j = 0; j < size; j++) {
-                 System.out.print(a[i][j]+" ");
-             }
-             System.out.println("\n");
-         }
-     }
-     public void getB(){
-         for (int i = 0; i < size; i++) {
-             System.out.print(b[i]+" ");
-         }
-         System.out.println("\n");
-     }
-     public void getX(){
-         for (int i = 0; i < size; i++) {
-             System.out.print(x[i]+" ");
-         }
-     }
-     public void setSize(int size){
+
+    public void calculate() {
+        for (int i = 0; i < size; i++) {
+            x[i] = b[i] / a[i][i];
+            for (int j = i + 1; j < size; j++) {
+                b[j] = b[j] - a[j][i] * x[i];
+            }
+        }
+    }
+
+    public void soutA() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+    public Float getA(int b,int c){
+        return a[b][c];
+    }
+
+    public void getB() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(b[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public void getX() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(x[i] + " ");
+        }
+    }
+
+    public void setSize(int size) {
         this.size = size;
-     }
+    }
+
+    public int getSize() {
+        return size;
+    }
 
     public static void main(String[] args) {
         Main main = new Main();
@@ -70,12 +85,12 @@ public class Main {
         main.setSize(scanner.nextInt());
         main.fill();
         main.zeroes();
-        main.getA();
+        //main.getA();
         main.getB();
         long start = System.nanoTime();
         main.calculate();
         long end = System.nanoTime();
-        long duration = (end-start);
+        long duration = (end - start);
         System.out.println(duration);
         main.getX();
     }
--- src/Ui.java
+++ src/Ui.java
@@ -9,51 +9,132 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Ui extends Application {
     Main main = new Main();
+    GridPane aMatrix;
+    GridPane bMatrix;
+    GridPane xMatrix;
+    private int size = 0;
+    private Float[][] a;
+    private Float[] b;
+    private Float[] x;
+    private int width;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         BorderPane borderPane = new BorderPane();
         borderPane.setPrefSize(1000, 800);
-        GridPane gridPane = new GridPane();
+        aMatrix = new GridPane();
+        bMatrix = new GridPane();
+        xMatrix = new GridPane();
 
         VBox vBox = new VBox();
-        TextArea size = new TextArea();
-        Button random = new Button("random");
-        random.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                main.fillRandom();
-            }
-        });
-        vBox.getChildren().add(random);
+        TextArea sizeArea = new TextArea();
+        sizeArea.setPrefSize(100,100);
+        TextArea widthArea = new TextArea();
+        sizeArea.setPrefSize(100,100);
         Button calculate = new Button("calculate");
         calculate.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                main.setSize(Integer.parseInt(size.getText()));
-                main.calculate();
+                a = new Float[size][size];
+                b = new Float[size];
+                x = new Float[size];
+                setMatrix();
+                calculate();
+                fillXTable();
             }
         });
-        Button fill = new Button("fill");
-        fill.setOnAction(new EventHandler<ActionEvent>() {
+        Button generate = new Button("generate tables");
+        generate.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                main.fill();
+                createTables(size);
             }
         });
-        vBox.getChildren().add(fill);
+        Button setSize = new Button("set size");
+        setSize.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                size = Integer.parseInt(sizeArea.getText());
+                width = Integer.parseInt(widthArea.getText());
+            }
+        });
         vBox.getChildren().add(calculate);
-        borderPane.setCenter(gridPane);
+        vBox.getChildren().add(setSize);
+        vBox.getChildren().add(sizeArea);
+        vBox.getChildren().add(generate);
+        vBox.getChildren().add(widthArea);
+        borderPane.setLeft(aMatrix);
+        borderPane.setCenter(bMatrix);
+        borderPane.setBottom(xMatrix);
         borderPane.setRight(vBox);
         Scene scene = new Scene(borderPane);
         primaryStage.setTitle("ZSI");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    public void createTables(int size){
+        for (int i = 0; i < size; i++) {
+            TextArea area1 = new TextArea();
+            TextArea area2 = new TextArea();
+            area1.setText("0");
+            area2.setText("0");
+            area1.setPrefSize(50,50);
+            area2.setPrefSize(100,50);
+            area2.setEditable(false);
+            bMatrix.add(area1,0,i);
+            xMatrix.add(area2,0,i);
+            for (int j = 0; j < size; j++) {
+                TextArea area = new TextArea();
+                area.setPrefSize(50,50);
+                area.setText("0");
+                aMatrix.add(area,i,j);
+            }
+        }
+    }
+    public void setMatrix(){
+        System.out.println("latafak");
+        TextArea aa;
+        for (int i = 0; i < size; i++) {
+            aa = (TextArea) bMatrix.getChildren().get(i);
+            b[i]=Float.parseFloat(aa.getText());
+            for (int j = 0; j < size; j++) {
+                aa = (TextArea) aMatrix.getChildren().get(j+(i*size));
+                a[j][i]=Float.parseFloat(aa.getText());
+            }
+        }
+    }
+    public void fillXTable(){
+        TextArea area;
+        for (int i = 0; i < size; i++) {
+            area = (TextArea) xMatrix.getChildren().get(i);
+            area.setText(Float.toString(x[i]));
+        }
+    }
+
+    public void calculate() {
+        for (int i = 0; i < size; i++) {
+            x[i] = b[i] / a[i][i];
+            for (int j = i + 1; j < width; j++) {
+                b[j] = b[j] - a[j][i] * x[i];
+            }
+        }
+    }
+    public void fillRandom() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                a[i][j] = ThreadLocalRandom.current().nextFloat() * ThreadLocalRandom.current().nextInt(1, 20);
+            }
+            b[i] = ThreadLocalRandom.current().nextFloat() * ThreadLocalRandom.current().nextInt(1, 20);
+        }
+    }
 
     public static void main(String[] args) {
         launch(Ui.class,args);
     }
 }
+
+//ustaw rozmiar x - tworzy siatke x - uzupelnij siatki x - pobierz wartosc z siatki do zmiennych x - calculate x - siatka zwrotna
